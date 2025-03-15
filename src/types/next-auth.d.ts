4d4ab9d5ba63c58b47eb100124abe46099a5d5d2
benjamin parker <benjamin.parker.us@gmail.com>
2025-03-14 18:36:28 -0600

    Fix unused import in next-auth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth"
+import type { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
-  interface Session {
+  interface Session extends DefaultSession {
     user: {
       id: string
       name: string
       email: string
       image: string
-    }
+    } & DefaultSession["user"]
   }
 } 
