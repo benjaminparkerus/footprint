ae7b1234ee5f4d1f2af8eeeed0441aab9895b166
benjamin parker <benjamin.parker.us@gmail.com>
2025-03-14 18:27:17 -0600

    Fix ESLint errors: Remove unused Image imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -77,5 +76,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-  );
+  )
 }
